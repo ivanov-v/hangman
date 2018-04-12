5fca454274f02a2aa50e45e43ba290d139cc6781
ivanov-v <sferamail33@yandex.ru>
2018-04-12 14:00:14 +0300

    рефакторинг

--- client-src/index.js
+++ client-src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware, compose} from 'redux';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
+import {
+    createStore,
+    applyMiddleware,
+    compose,
+} from 'redux';
 
 import {App} from './components/App';
 import {rootReducer} from './reducers';
-import {getInitialIssue} from './actions'
+import {getInitialIssue} from './actions';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
